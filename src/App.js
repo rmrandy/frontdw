961ed8729e2022301ab664e793ad3ede247a8b14
Randy Rivera <rr36693904@gmail.com>
2023-09-28 01:44:26 -0600

    Modulo de Login y navbar para la creación de articulos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Inicio from './Inicio' 
+import LogIn from './LogIn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/Login" component={LogIn} />
+        <Route exact path="/Inicio" component={Inicio} />
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/Inicio.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Inicio() {
+  return <div>Este es el inicio</div>;
+}
+
+export default Inicio;
