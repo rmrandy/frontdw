7feeb398d37cf5ac7cf2e81bcb2a28359993f8c1
Randy Rivera <rr36693904@gmail.com>
2023-09-28 18:47:13 -0600

    LogIn - SingIn para el home

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,43 +3,48 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Redirect,
-    useParams
+    // Redirect,
+    // useParams
 } from "react-router-dom"
 import { LoginContext } from "../context/contexto"; //para usar variables de usuario y tipo en la app
 import SignUp from "../components/SignUp/SignUp";
 import { useContext } from "react";
+import SignIn from "../components/LogIn/SignIn";
+
 
 export default function AppRouter(){
     
     const{
-        username, setUsername, setTipoUsuario, tipoUsuario
+        // username, setUsername, setTipoUsuario, 
+        tipoUsuario
     } = useContext(LoginContext);
 
     
     
     return(
         <Router>
-            {tipoUsuario == 1 ?
+            {tipoUsuario === 1 ?
             <Switch>
             <Route path = "/signUp" component={SignUp}/>
 
             </Switch> :
 
-            tipoUsuario == 2?
+            tipoUsuario === 2?
             <Switch>
             <Route path = "/signUp" component={SignUp}/>
 
         </Switch> :
-        tipoUsuario == 3?
+        tipoUsuario === 3?
         <Switch>
         <Route path = "/signUp" component={SignUp}/>
         
     </Switch> :
-     <Switch>
-                <Route path = "/signUp" component={SignUp}/>
 
-            </Switch>
+
+     <Switch>
+      <Route path = "/signUp" component={SignUp}/>
+      <Route path = "/singIn" component={SignIn} />
+     </Switch>
             }
         </Router>
     );
