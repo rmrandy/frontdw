d09107cdf9e2e1ba088bb9481044485d4ab41681
Randy Rivera <rr36693904@gmail.com>
2023-10-10 22:58:51 -0600

    Plantilla 2

--- src/components/Prueba/prueba.js
+++ src/components/Prueba/prueba.js
@@ -1,35 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Importa Link desde react-router-dom
 import foto from "../Home/assets/4.jpg";
 import foto2 from "../Home/assets/0FD9ABA0-E3E6-461C-B06A-DF38B7D0D995.jpg";
 import foto3 from "../Home/assets/BB3.jpg";
-import "./prueba.css"; 
+import "./prueba.css";
 
 const Categorias = () => {
   return (
-    <div className="categorias-container" style={{ width: "100%", height: "700px%" }}>
+    <div className="categorias-container" style={{ height: "100vh" }}>
       <div className="category">
-        <div className="category-content">
-          <div className="category-image">
-          <img
-              src={foto2}
-              alt="Imagen"
-              style={{
-                borderRadius: "20px",
-                width: "350px",
-                height: "500px",
-                objectFit: "cover",
-                display: "block",
-                margin: "0 auto",
-                marginTop: "30px",
-                marginBottom: "60px"
-              }}
-            />
+        <Link to="/plantilla_noticia" style={{ textDecoration: "none" }}>
+          <div className="category-content">
+            <div className="category-image">
+              <img
+                src={foto2}
+                alt="Imagen"
+                style={{
+                  borderRadius: "20px",
+                  width: "350px",
+                  height: "500px",
+                  objectFit: "cover",
+                  display: "block",
+                  margin: "0 auto",
+                  marginTop: "30px",
+                  marginBottom: "60px",
+                }}
+              />
+            </div>
+            <div className="category-text">
+              <h1>Tu título aquí</h1>
+              <p>Contenido de la categoría "Noticia"</p>
+            </div>
           </div>
-          <div className="category-text">
-            <h1>Tu título aquí</h1>
-            <p>Contenido de la categoría "Noticia"</p>
-          </div>
-        </div>
+        </Link>
       </div>
 
       <div className="category">
@@ -46,7 +49,7 @@ const Categorias = () => {
                 display: "block",
                 margin: "0 auto",
                 marginTop: "30px",
-                marginBottom: "60px"
+                marginBottom: "60px",
               }}
             />
           </div>
@@ -55,20 +58,24 @@ const Categorias = () => {
             <p>Contenido de la categoría "Opinión"</p>
           </div>
           <div className="category-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-crown"
-            >
-              <path d="m2 4 3 12h14l3-12-6 7-4-7-4 7-6-7zm3 16h14" />
-            </svg>
+            <Link to="/plantilla_noticia">
+              {" "}
+              {/* Enlace al componente "PlantillaNoticia" */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-crown"
+              >
+                <path d="m2 4 3 12h14l3-12-6 7-4-7-4 7-6-7zm3 16h14" />
+              </svg>
+            </Link>
           </div>
         </div>
       </div>
@@ -87,14 +94,13 @@ const Categorias = () => {
                 display: "block",
                 margin: "0 auto",
                 marginTop: "30px",
-                marginBottom: "60px"
-                
+                marginBottom: "60px",
               }}
             />
           </div>
           <div className="category-text">
             <h1>Tu título aquí</h1>
-            <p> Contenido de la categoría "Reseñas"</p>
+            <p>Contenido de la categoría "Reseñas"</p>
           </div>
         </div>
       </div>
